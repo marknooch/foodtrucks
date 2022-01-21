4e3eef2dd8fee3086d33b310f337576c4fb3920b
Mark <mark@isitonmyend.com>
2022-01-21 16:08:09 -0700

    use a constant for paths

--- .github/workflows/createGeoJson.ps1
+++ .github/workflows/createGeoJson.ps1
@@ -2,14 +2,16 @@ Install-Module JoinModule -RequiredVersion 3.6.0 -Force  # given more time, we'd
 new-item "../../data" -itemtype Container -Force
 
 # get the permits file and filter to ones that are approved
-Invoke-WebRequest -Uri "https://data.sfgov.org/api/views/rqzj-sfat/rows.csv" -OutFile ./permits.csv
-$permits = Import-Csv "../../data/permits.csv" | where-object { $_.Status -eq "APPROVED" }  
+$permitspath = "../../data/permits.csv"
+Invoke-WebRequest -Uri "https://data.sfgov.org/api/views/rqzj-sfat/rows.csv" -OutFile $permitspath
+$permits = Import-Csv $permitspath | where-object { $_.Status -eq "APPROVED" }  
 write-host "Downloaded and imported the permits"
 
 # get the schedules file and filter to ones that are open now
-Invoke-WebRequest -Uri "https://data.sfgov.org/api/views/jjew-r69b/rows.csv" -OutFile ./schedules.csv
+$schedulespath = "../../data/schedules.csv"
+Invoke-WebRequest -Uri "https://data.sfgov.org/api/views/jjew-r69b/rows.csv" -OutFile $schedulespath
 $dayOfWeek = (get-date).DayOfWeek
-$schedules = import-csv "../../data/schedules.csv" | where-object { $_.DayOfWeekStr -eq $dayOfWeek }
+$schedules = import-csv $schedulespath | where-object { $_.DayOfWeekStr -eq $dayOfWeek }
 $schedulesNo2400s = foreach ($schedule in $schedules) {
     if($schedule.end24 -eq "24:00") {$schedule.end24 = "23:59"}
     $schedule
@@ -18,11 +20,13 @@ $ScheduledFoodTrucks = $schedulesNo2400s | Where-Object {$_.start24 -le $timeOfD
 write-host "Downloaded and imported the schedules"
 
 # join the two objects together to find all of the approved food trucks that are open right now --- save that output
+$openfoodtruckspath = "../../data/openfoodtrucks.csv"
 $openFoodTrucks = $permits | InnerJoin-Object $ScheduledFoodTrucks -On permit
-Export-Csv -InputObject $openFoodTrucks -Path "../../data/openfoodtrucks.csv"
+Export-Csv -InputObject $openFoodTrucks -Path $openfoodtruckspath
 write-host "created the open food trucks"
 
 # now produce the GeoJSON
+$openfoodtrucksjson = "../../data/openFoodTrucks.json"
 $features = foreach ($foodTruck in $openFoodTrucks) {
     [string[]]$coordinates = @()
     $coordinates+= $foodTruck.Longitude[0]
@@ -40,5 +44,5 @@ $featureCollection = @{
     type = "FeatureCollection"
     features = $features
 }
-ConvertTo-Json -InputObject $featureCollection -Depth 100 > "../../data/openFoodTrucks.json"
+ConvertTo-Json -InputObject $featureCollection -Depth 100 > $openfoodtrucksjson
 write-host "produced the open food trucks json"
