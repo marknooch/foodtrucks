36ec9bfe1da7e453297755f356300b3a201c2458
Mark <mark@isitonmyend.com>
2022-01-22 14:30:34 -0700

    Revert "Hopefully this will apply to the bucekt level now"
    This reverts commit 8db32445e5884959204f6665c776a8a44be8c607.

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,7 +39,7 @@ data "aws_iam_policy_document" "public-read" {
     }
 
     actions = [ "s3:GetObject", "s3:GetObjectVersion", "s3:ListBucket"]
-    resources = [aws_s3_bucket.s3_home.arn]
+    resources = ["${aws_s3_bucket.s3_home.arn}/*"]
   }
 }
 
