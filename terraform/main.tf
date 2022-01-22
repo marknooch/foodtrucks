5cf02bca3bf3fa75b27f7392d2bb41f7f612eaef
Mark <mark@isitonmyend.com>
2022-01-22 14:31:11 -0700

    remove listbucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,7 +38,7 @@ data "aws_iam_policy_document" "public-read" {
       identifiers = [ "*" ]
     }
 
-    actions = [ "s3:GetObject", "s3:GetObjectVersion", "s3:ListBucket"]
+    actions = [ "s3:GetObject", "s3:GetObjectVersion"]
     resources = ["${aws_s3_bucket.s3_home.arn}/*"]
   }
 }
