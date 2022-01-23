6b35cbc5eb842f17aaceec10fe610eebab86c02e
Mark <mark@isitonmyend.com>
2022-01-23 10:57:12 -0700

    produce a certificate using ACM

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -2,17 +2,34 @@ resource "aws_route53_zone" "main" {
   name = var.domain-name
 }
 
-# comment out so we can get the zone and configure name records in freenom before getting the rest of this stuff setup
+resource "aws_route53_record" "ssl-cert-validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.ssl-certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.main.zone_id
+}
+
 # resource "aws_route53_record" "root-a" {
 #   zone_id = aws_route53_zone.main.zone_id
 #   name    = var.domain-name
 #   type    = "A"
 
-#   alias {
-#     name                   = aws_cloudfront_distribution.root_s3_distribution.domainvar.domain-name
-#     zone_id                = aws_cloudfront_distribution.root_s3_distribution.hosted_zone_id
-#     evaluate_target_health = false
-#   }
+#   # comment out   
+#   #   alias {
+#   #     name                   = aws_cloudfront_distribution.root_s3_distribution.domainvar.domain-name
+#   #     zone_id                = aws_cloudfront_distribution.root_s3_distribution.hosted_zone_id
+#   #     evaluate_target_health = false
+#   #   }
 # }
 
 # resource "aws_route53_record" "www-a" {
@@ -20,9 +37,9 @@ resource "aws_route53_zone" "main" {
 #   name    = "www.${var.domain-name}"
 #   type    = "A"
 
-#   alias {
-#     name                   = aws_cloudfront_distribution.www_s3_distribution.domainvar.domain-name
-#     zone_id                = aws_cloudfront_distribution.www_s3_distribution.hosted_zone_id
-#     evaluate_target_health = false
-#   }
+#   #   alias {
+#   #     name                   = aws_cloudfront_distribution.www_s3_distribution.domainvar.domain-name
+#   #     zone_id                = aws_cloudfront_distribution.www_s3_distribution.hosted_zone_id
+#   #     evaluate_target_health = false
+#   #   }
 # }
