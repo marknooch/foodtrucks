32f6aa7e494d1d0b399dc1ff7914fe2d181bd097
Mark <mark@isitonmyend.com>
2022-01-23 11:58:17 -0700

    start up cloudfront

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,7 +1,14 @@
 # create bucket for static website
 resource "aws_s3_bucket" "s3-home" {
-  bucket_prefix = "foodtrucks"
+  bucket_prefix = var.bucket-prefix
   acl           = "public-read"
+
+  cors_rule {
+    allowed_headers = ["Authorization", "Content-Length"]
+    allowed_methods = ["GET", "POST"]
+    allowed_origins = ["https://www.${var.domain-name}"]
+    max_age_seconds = 3000
+  }
 
   website {
     index_document = "index.html"
@@ -35,4 +42,42 @@ data "aws_iam_policy_document" "s3-home-ipd" {
 resource "aws_s3_bucket_policy" "public-read" {
   bucket = aws_s3_bucket.s3-home.id
   policy = data.aws_iam_policy_document.s3-home-ipd.json
+}
+
+# S3 bucket for redirecting non-www to www.
+resource "aws_s3_bucket" "s3-redirect" {
+  bucket_prefix = var.bucket-prefix
+  acl           = "public-read"
+
+  website {
+    redirect_all_requests_to = "https://www.${var.domain-name}"
+  }
+
+}
+
+data "aws_iam_policy_document" "s3-redirect-ipd" {
+  statement {
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions   = ["s3:GetObject", "s3:GetObjectVersion"]
+    resources = ["${aws_s3_bucket.s3-home.arn}/*"]
+  }
+
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_user.github-actions.arn]
+    }
+
+    actions   = ["s3:*"]
+    resources = ["${aws_s3_bucket.s3-redirect.arn}/*"]
+  }
+}
+
+resource "aws_s3_bucket_policy" "s3-redirect-policy" {
+  bucket = aws_s3_bucket.s3-home.id
+  policy = data.aws_iam_policy_document.s3-redirect-ipd.json
 }
