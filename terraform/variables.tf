3f544a1bd361c35f1d3d2f1a2519d275518c68e1
Mark <mark@isitonmyend.com>
2022-01-23 09:14:32 -0700

    add a default tag to resources

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,16 @@ variable "region" {
     description = "AWS region which will host our resources"
     default = "us-east-2"
 }
+
+variable "domain-name" {
+    description = "domain name which redirects to CloudFront"
+    default = "findafoodtruck.ga"
+}
+
+variable "tags" {
+    description = "tags to apply to the AWS resources"
+    default = {
+        project = "findafoodtruck"
+    }
+    type = map(string)
+}
